58be657c0b28c9b459233f8cea8936e7a26484bd
Apple Kun <101918076+Pop-Apple@users.noreply.github.com>
2022-07-13 19:04:53 +0900

    Add files via upload

---
+++ Simple-Progress/Simple-Progress/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Simple_Progress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(41, 55);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(481, 30);
+            this.progressBar.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(41, 107);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(123, 49);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(190, 107);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(123, 49);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(38, 17);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(124, 18);
+            this.label.TabIndex = 3;
+            this.label.Text = "Processing... 0%";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 180);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SImple Progressbar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Simple-Progress/Simple-Progress/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Simple_Progress
+{
+    public partial class Form1 : Form
+    {
+        private const string V = "Processing";
+
+        public Form1(){
+            InitializeComponent();
+        }
+        private void Successfully(){
+            MessageBox.Show("100% Successfully", "Simple Progress", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+        private void Form1_Load(object sender, EventArgs e){
+            this.btnStop.Enabled = false;
+            this.progressBar.Maximum = 100;
+        }
+        private void btnStart_Click(object sender, EventArgs e){
+            this.timer.Start();
+            this.btnStart.Enabled = false;
+            this.btnStop.Enabled = true;
+        }
+        private void btnStop_Click(object sender, EventArgs e){
+            this.timer.Stop();
+            this.btnStart.Enabled = true;
+            this.btnStop.Enabled = false;
+        }
+        private void timer_Tick(object sender, EventArgs e){
+            this.progressBar.Increment(1);
+            this.progressBar.Value.ToString();
+            this.label.Text = "Processing... " + progressBar.Value.ToString() + "%";
+            bool Processing = this.progressBar.Value == 100;
+            if (Processing){
+                timer.Stop();
+                Successfully();
+                this.progressBar.Value = 0;
+                this.label.Text = "Processing... 0%";
+                this.btnStart.Enabled = true;
+                this.btnStop.Enabled = false;
+            }
+        }
+    }
+}
